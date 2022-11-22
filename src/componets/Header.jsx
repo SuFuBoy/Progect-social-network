5e8b8fd2ca52f71f723f2ce183ca6e7ad995cf77
SuFu <saifai.sf35@gmail.com>
2022-11-22 17:23:49 +0300

    Настроил css модуль

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './Header.module.css'
 
 const Header = () => {
-    return <header className='header'>
-      <img src="https://w7.pngwing.com/pngs/79/518/png-transparent-js-react-js-logo-react-react-native-logos-icon-thumbnail.png"></img>
-    </header>
+  return <header className={s.header}>
+    <img src="https://w7.pngwing.com/pngs/79/518/png-transparent-js-react-js-logo-react-react-native-logos-icon-thumbnail.png"></img>
+  </header>
 };
 
 export default Header;
--- src/componets/Profile.jsx
+++ src/componets/Profile.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 const Profile = () => {
-    return <div className='Profile'>
-        <div>
-        <img src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'/>
+  return <div className={s.profile}>
+    <div>
+      <img src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300' />
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        New posts
+        <div className={s.posts}>
+          <div className={s.item}>Post1</div>
+          <div className={s.item}>Post2</div>
         </div>
-        <div>
-          ava + description
-        </div>
-        <div>
-          My posts
-          <div>
-            New posts
-            <div>
-              <div>Post1</div>
-              <div>Post2</div>
-            </div>
-          </div>
-        </div> 
       </div>
+    </div>
+  </div>
 }
 
 export default Profile;
